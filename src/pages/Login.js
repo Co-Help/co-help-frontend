5b08894dc05b8068229d4798897e270bd75a4aca
DNI9 <itsr9boi@gmail.com>
2021-05-25 18:53:48 +0530

    initial login ui

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,3 +1,33 @@
+import {Button} from '@chakra-ui/button';
+import {Box, Flex, Heading, Text} from '@chakra-ui/layout';
+import {ImGoogle} from 'react-icons/im';
+
 export const Login = () => {
-  return <div>LOGIN</div>;
+  return (
+    <Flex bg='#DBE2EF' minH='100vh' minW='full'>
+      <Flex
+        justify='center'
+        align='center'
+        w='full'
+        bg='#FCFDFF'
+        boxShadow='lg'
+        p={5}>
+        <Button
+          bg='#3F72AF'
+          _hover={{background: '#3F72AF'}}
+          p={6}
+          color='white'
+          leftIcon={<ImGoogle color='white' size='20' />}>
+          Login with Google
+        </Button>
+      </Flex>
+
+      <Box display={['none', 'block']} p={5} py={10} w='full'>
+        <Heading>Co-Help</Heading>
+        <Text fontSize='xl'>
+          Find doctors, beds, oxygen and other medical needs.
+        </Text>
+      </Box>
+    </Flex>
+  );
 };
