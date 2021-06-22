5e2b6f76689c10a4baf3cf479b9f7ca112e71eac
DNI9 <itsr9boi@gmail.com>
2021-06-22 21:27:03 +0530

    show alert if profile is not completed

--- src/pages/user/UserProfile.js
+++ src/pages/user/UserProfile.js
@@ -1,37 +1,67 @@
 import {Avatar} from '@chakra-ui/avatar';
 import {Box, Center, Container, Heading, Text} from '@chakra-ui/layout';
-import {Tab, TabList, TabPanel, TabPanels, Tabs} from '@chakra-ui/react';
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  Button,
+  Link,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from '@chakra-ui/react';
 import {useSelector} from 'react-redux';
+import {Link as RLink} from 'react-router-dom';
 import {BookingsTabPanel} from './components/BookingsTabPanel';
 import {OthersTabPanel} from './components/OthersTabPanel';
 
 export const UserProfile = () => {
-  const {name, email, avatar} = useSelector(state => state.user.profile);
+  const {name, email, avatar, is_profile_completed} = useSelector(
+    state => state.user.profile
+  );
 
   return (
-    <Container mt={5} px={5} py={10}>
-      <Center py={5} flexDirection='column'>
-        <Avatar src={avatar} name={name ?? 'User'} size='2xl' />
-        <Heading pt={2}>{name}</Heading>
-        <Text>{email}</Text>
-      </Center>
-      <Box my={5}>
-        <Tabs isLazy isFitted>
-          <TabList>
-            <Tab>Bookings</Tab>
-            <Tab>Others</Tab>
-          </TabList>
+    <>
+      {!is_profile_completed && (
+        <Alert rounded='sm' size='md' status='warning'>
+          <AlertIcon />
+          <AlertDescription>
+            Seems you haven't completed your profile, please complete to get
+            better search results
+          </AlertDescription>
+          <Link ml='auto' as={RLink} to='/user/complete-profile'>
+            <Button colorScheme='blackAlpha' size='sm'>
+              Complete now
+            </Button>
+          </Link>
+        </Alert>
+      )}
+      <Container mt={5} px={5} py={10}>
+        <Center py={5} flexDirection='column'>
+          <Avatar src={avatar} name={name ?? 'User'} size='2xl' />
+          <Heading pt={2}>{name}</Heading>
+          <Text>{email}</Text>
+        </Center>
+        <Box my={5}>
+          <Tabs isLazy isFitted>
+            <TabList>
+              <Tab>Bookings</Tab>
+              <Tab>Others</Tab>
+            </TabList>
 
-          <TabPanels>
-            <TabPanel>
-              <BookingsTabPanel />
-            </TabPanel>
-            <TabPanel>
-              <OthersTabPanel />
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </Box>
-    </Container>
+            <TabPanels>
+              <TabPanel>
+                <BookingsTabPanel />
+              </TabPanel>
+              <TabPanel>
+                <OthersTabPanel />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Box>
+      </Container>
+    </>
   );
 };
