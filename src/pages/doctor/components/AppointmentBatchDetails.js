94b9cac42fd0c9204ef4a565979db1ab61bb18b0
DNI9 <itsr9boi@gmail.com>
2021-06-24 14:38:13 +0530

    show only active appointments

--- src/pages/doctor/components/AppointmentBatchDetails.js
+++ src/pages/doctor/components/AppointmentBatchDetails.js
@@ -24,31 +24,33 @@ export const AppointmentBatchDetails = () => {
   return (
     <>
       <AppointmentTable title='Appointment details'>
-        {appointmentBatch?.map((ap, idx) => (
-          <Tr key={ap._id}>
-            <Td>{idx + 1}</Td>
-            <Td>{ap.appointment_date.split('T')[0]}</Td>
-            <Td isNumeric>{ap.cost}</Td>
-            <Td>{ap.info || 'No info'}</Td>
-            <Td>
-              {ap.booked ? (
-                <IconButton
-                  onClick={() => dispatch(setAppointmentDone(ap._id))}
-                  aria-label='Set done'
-                  title='Set done'
-                  icon={<CheckCircleIcon />}
-                />
-              ) : (
-                <IconButton
-                  onClick={() => dispatch(deleteAppointment(ap._id))}
-                  aria-label='Delete appointment'
-                  title='Delete appointment'
-                  icon={<DeleteIcon color='red.500' />}
-                />
-              )}
-            </Td>
-          </Tr>
-        ))}
+        {appointmentBatch
+          ?.filter(ap => !ap.done)
+          .map((ap, idx) => (
+            <Tr key={ap._id}>
+              <Td>{idx + 1}</Td>
+              <Td>{ap.appointment_date.split('T')[0]}</Td>
+              <Td isNumeric>{ap.cost}</Td>
+              <Td>{ap.info || 'No info'}</Td>
+              <Td>
+                {ap.booked ? (
+                  <IconButton
+                    onClick={() => dispatch(setAppointmentDone(ap._id))}
+                    aria-label='Set done'
+                    title='Set done'
+                    icon={<CheckCircleIcon />}
+                  />
+                ) : (
+                  <IconButton
+                    onClick={() => dispatch(deleteAppointment(ap._id))}
+                    aria-label='Delete appointment'
+                    title='Delete appointment'
+                    icon={<DeleteIcon color='red.500' />}
+                  />
+                )}
+              </Td>
+            </Tr>
+          ))}
       </AppointmentTable>
       {!appointmentBatch?.length && (
         <Text textAlign='center' mt={15}>
